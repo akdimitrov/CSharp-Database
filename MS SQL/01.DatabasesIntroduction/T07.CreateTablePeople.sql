e3134c14b026dee6e9319d6a346b6319b3dd1345
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-05-21 00:53:06 +0300

    Added problem solution

--- MS SQL/01.DatabasesIntroduction/T07.CreateTablePeople.sql
+++ MS SQL/01.DatabasesIntroduction/T07.CreateTablePeople.sql
@@ -1,20 +1,20 @@
 CREATE TABLE People
 (
-	Id Int PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	[Name] NVARCHAR(200) NOT NULL,
-	Picture VARBINARY(MAX) CHECK (DATALENGTH(Picture) <= 2097152),
-	Height FLOAT(2),
-	[Weight] FLOAT(2),
-	Gender CHAR(1) CHECK (gender = 'f' or gender = 'm') NOT NULL,
-	Birthdate DATETIME NOT NULL,
-	Biography NVARCHAR(MAX)
+	[Picture] VARBINARY(MAX) CHECK (DATALENGTH([Picture]) <= 2097152),
+	[Height] DECIMAL(3,2),
+	[Weight] DECIMAL(5,2),
+	[Gender] CHAR(1) CHECK ([Gender] = 'f' OR [Gender] = 'm') NOT NULL,
+	[Birthdate] DATE NOT NULL,
+	[Biography] NVARCHAR(MAX)
 )
 
-INSERT INTO People ([Name], Gender, Birthdate) VALUES
-('Ivan', 'm', 28-11-1992),
-('Georgre', 'm', 15-10-1991),
-('Floo', 'f', 5-7-1990),
-('Madelane', 'f', 3-12-1994),
-('Christina', 'f', 26-9-1996)
+INSERT INTO People ([Name], [Gender], [Birthdate]) VALUES
+('Ivan', 'm', '1992-11-28'),
+('Georgre', 'm', '1991-10-15'),
+('Floo', 'f', '1992-07-09'),
+('Madelane', 'f', '1994-12-03'),
+('Christina', 'f', '1996-09-26')
 
 DROP TABLE People
