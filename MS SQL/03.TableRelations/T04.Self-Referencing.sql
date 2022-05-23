ea5b5408828b61598b9b507dd89790a014df6f1b
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-05-23 23:15:18 +0300

    Added problem solution

---
+++ MS SQL/03.TableRelations/T04.Self-Referencing.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Teachers
+(
+	TeacherID INT PRIMARY KEY IDENTITY(101,1),
+	[Name] NVARCHAR(30) NOT NULL,
+	ManagerID INT REFERENCES Teachers(TeacherID)
+)
+GO
+
+INSERT INTO Teachers([Name], ManagerID)
+	VALUES
+	('John', NULL),
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101)
+GO
+
+SELECT * FROM Teachers
