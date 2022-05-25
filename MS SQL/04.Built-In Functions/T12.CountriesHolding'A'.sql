65486b6d92499b3320c0123fc64eaa26fa182d35
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-05-25 04:15:33 +0300

    Added problem solution

---
+++ MS SQL/04.Built-In Functions/T12.CountriesHolding'A'.sql
@@ -0,0 +1,9 @@
+SELECT CountryName, IsoCode
+	FROM Countries
+	WHERE (LEN(CountryName) - LEN(REPLACE(CountryName, 'a' ,''))) >= 3
+	ORDER BY IsoCode
+
+SELECT CountryName, IsoCode
+	FROM Countries
+	WHERE CountryName LIKE '%a%a%a%'
+	ORDER BY IsoCode
