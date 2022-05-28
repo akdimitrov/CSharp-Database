ca58c830990c1bef6e70203c27f6d0b6b2fb1667
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-05-28 15:35:36 +0300

    Added problem solution

---
+++ MS SQL/05.Subqueries and Joins/T08.Employee24.sql
@@ -0,0 +1,10 @@
+SELECT e.EmployeeID, 
+       e.FirstName,
+	   CASE
+		   WHEN p.StartDate >= '2005' THEN NULL
+		   ELSE p.[Name] 
+	   END AS ProjectName 
+	FROM Employees e
+	JOIN EmployeesProjects ep ON ep.EmployeeID = e.EmployeeID
+	JOIN Projects p ON p.ProjectID = ep.ProjectID
+	WHERE e.EmployeeID = 24
