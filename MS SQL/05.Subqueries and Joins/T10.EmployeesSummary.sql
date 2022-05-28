abb5d7c5421c478c93a1af0933455cdc6952c630
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-05-28 15:49:31 +0300

    Added problem solution

---
+++ MS SQL/05.Subqueries and Joins/T10.EmployeesSummary.sql
@@ -0,0 +1,9 @@
+SELECT TOP(50)
+	   e.EmployeeID, 
+	   CONCAT_WS(' ', e.FirstName, e.LastName) AS EmployeeName,
+	   CONCAT_WS(' ', m.FirstName, m.LastName) AS ManagerName,
+	   d.[Name] AS DepartmentName
+	FROM Employees e
+	JOIN Employees m ON m.EmployeeID = e.ManagerID
+	JOIN Departments d ON d.DepartmentID = e.DepartmentID
+	ORDER BY EmployeeID
