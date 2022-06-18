12ce48a2e8bab53cc52c8c686eb977dd97ca4839
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-06-19 00:10:27 +0300

    Added problem solution

---
+++ MS SQL/08.Triggers and Transactions/T09.DeleteEmployees.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Deleted_Employees
+(
+	EmployeeId INT PRIMARY KEY,
+	FirstName NVARCHAR(30),
+	LastName NVARCHAR(30),
+	MiddleName NVARCHAR(30),
+	JobTitle NVARCHAR(30),
+	DepartmentId INT,
+	Salary DECIMAL (15,2)
+)
+GO
+
+CREATE TRIGGER tr_DeletedEmployees ON Employees FOR DELETE
+AS
+	INSERT INTO Deleted_Employees (EmployeeId, FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary)
+	SELECT EmployeeID, FirstName, LastName, MiddleName, JobTitle, DepartmentID, Salary FROM deleted
+GO
