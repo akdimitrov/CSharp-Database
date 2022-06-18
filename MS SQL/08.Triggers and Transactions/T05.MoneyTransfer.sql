5b0ae7b06fefab93d446e642fbf4a3ad0b4252cd
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-06-18 20:11:00 +0300

    Added problem solution

---
+++ MS SQL/08.Triggers and Transactions/T05.MoneyTransfer.sql
@@ -0,0 +1,6 @@
+CREATE PROC usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(18,4))
+AS
+BEGIN TRANSACTION
+	EXEC usp_WithdrawMoney @SenderId, @Amount
+	EXEC usp_DepositMoney @ReceiverId, @Amount
+COMMIT
