d345b82caaec9faef90545ab4412e95a5d150c9b
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-06-18 19:50:23 +0300

    Added problem solution

--- MS SQL/08.Triggers and Transactions/T04.WithdrawMoneyProcedure.sql
+++ MS SQL/08.Triggers and Transactions/T04.WithdrawMoneyProcedure.sql
@@ -1,4 +1,4 @@
-CREATE PROC usp_DepositMoney(@AccountId INT, @MoneyAmount DECIMAL(18,4))
+CREATE PROC usp_WithdrawMoney(@AccountId INT, @MoneyAmount DECIMAL(18,4))
 AS
 BEGIN TRANSACTION
 	IF (@MoneyAmount < 0)
@@ -13,8 +13,16 @@ BEGIN TRANSACTION
 			THROW 50002, 'Invalid account', 16
 		END
 	
+	IF ((SELECT Balance FROM Accounts WHERE @AccountId = Id) - @MoneyAmount < 0)
+		BEGIN
+			ROLLBACK;
+			THROW 50003, 'Not enough amount', 16
+		END
+
 	UPDATE Accounts
-		SET Balance += ROUND(@MoneyAmount, 4)
+		SET Balance -= ROUND(@MoneyAmount, 4)
 		WHERE Id = @AccountId
 	COMMIT
 GO
+
+SELECT * FROM Accounts
