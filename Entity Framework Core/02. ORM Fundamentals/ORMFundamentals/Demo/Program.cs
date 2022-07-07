1bc1342be74f44730031ed133b52f8635e2edaf0
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-07-07 23:58:48 +0300

    Added ORM demo

---
+++ Entity Framework Core/02. ORM Fundamentals/ORMFundamentals/Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Demo.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+            Console.WriteLine(db.Employees.Count());
+        }
+    }
+}
