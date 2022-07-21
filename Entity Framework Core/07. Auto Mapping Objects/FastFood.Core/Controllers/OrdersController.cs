74e52fd8bebc12c68606391ec1445424c1683d5f
Aleksandar Dimitrov <alexkdimitrov@gmail.com>
2022-07-21 23:55:51 +0300

    Initial commit

---
+++ Entity Framework Core/07. Auto Mapping Objects/FastFood.Core/Controllers/OrdersController.cs
@@ -0,0 +1,62 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Orders;
+
+    public class OrdersController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public OrdersController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var viewOrder = new CreateOrderViewModel
+            {
+                Items = context.Items.Select(x => x.Id).ToList(),
+                Employees = context.Employees.Select(x => x.Id).ToList(),
+            };
+
+            return View(viewOrder);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var order = mapper.Map<Order>(model);
+            var orderItem = mapper.Map<OrderItem>(model);
+
+            order.OrderItems.Add(orderItem);
+            context.Orders.Add(order);
+
+            context.SaveChanges();
+
+            return RedirectToAction("All", "Orders");
+        }
+
+        public IActionResult All()
+        {
+            var orders = context.Orders
+                .ProjectTo<OrderAllViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return View(orders);
+        }
+    }
+}
---
+++ Entity Framework Core/07. Auto Mapping Objects/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=.;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
